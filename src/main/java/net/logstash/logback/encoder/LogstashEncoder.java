462f6172918dc4dd5d91f6966678a201b342a267
John E. Vincent <lusis.org+github.com@gmail.com>
2013-02-26 14:55:33 -0500

    migrating to the logstash coordinates on central

--- src/main/java/net/logstash/logback/encoder/LogstashEncoder.java
+++ src/main/java/net/logstash/logback/encoder/LogstashEncoder.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.github.neilprosser.logstash;
+package net.logstash.logback.encoder;
 
 import static org.apache.commons.io.IOUtils.*;
 
