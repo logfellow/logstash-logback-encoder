36de162a04f1efa728f2b918f544cbaea61349a5
Ben Kirwin <ben@kirw.in>
2013-06-25 18:34:16 -0400

    Implement socket appender on top of the syslog implementation
    It turns out that the syslog base is not syslog-specific, but just ships
    the data in any given layout over UDP. (The facilty / severity are not
    used in the default implementation.)
    
    If you configure logstash like so:
    
        input {
          udp {
            port => 5515
            format => json_event
            tag => my_tag_for_logback_events
          }
        }
    
    and logback like so:
    
        <appender name="LOGSTASH" class="net.logstash.logback.appender.LogstashSocketAppender">
          <host>my.logstash.server.local</host>
          <port>5515</port>
        </appender>
    
    log messages will be sent directly to the server, with no need for a
    logstash daemon running locally.

---
+++ src/main/java/net/logstash/logback/appender/LogstashSocketAppender.java
@@ -0,0 +1,48 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.logstash.logback.appender;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.net.SyslogAppenderBase;
+import net.logstash.logback.layout.LogstashLayout;
+
+public class LogstashSocketAppender extends SyslogAppenderBase<ILoggingEvent> {
+
+    @Override
+    public Layout<ILoggingEvent> buildLayout() {
+        return new LogstashLayout();
+    }
+
+    public LogstashSocketAppender() {
+        setFacility("NEWS"); // NOTE: this value is never used
+    }
+
+    @Override
+    public int getSeverityForEvent(Object eventObject) {
+        return 0; // NOTE: this value is never used
+    }
+
+    public String getHost() {
+        return getSyslogHost();
+    }
+
+    /**
+     * Just an alias for syslogHost (since that name doesn't make much sense here)
+     * @param host
+     */
+    public void setHost(String host) {
+        setSyslogHost(host);
+    }
+}
