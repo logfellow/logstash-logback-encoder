76b06ad46076a4e5a6767950567ebd98efbf391d
Dmitry Sklyut <dmitry@dsklyut.com>
2014-03-24 09:51:35 -0400

    Fix osgi imports because of shade plugin relocation

--- src/main/java/net/logstash/logback/LogstashFormatter.java
+++ src/main/java/net/logstash/logback/LogstashFormatter.java
@@ -30,13 +30,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.commons.lang.time.FastDateFormat;
-import org.slf4j.Marker;
-
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * 
