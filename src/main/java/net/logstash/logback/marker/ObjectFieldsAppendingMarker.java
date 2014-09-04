ca3f0769bb7afba2944c1e79cc3e34168df0a4eb
Phil Clay <pclay@blizzard.com>
2014-09-04 11:24:12 -0700

    Improve the performance of ObjectFieldsAppendingMarker by removing need to convert the object to a JsonNode tree prior to writing the tree.
    Instead, now we use an UnwrappingBeanSerializer to write the object directly
    to the JsonGenerator (without having to convert to JsonNodes first).
    
    Now, the ObjectFieldsAppendingMarker is actually faster than the MapEntriesAppendingMarker
    when you want to embed fields in the logstash event json.

--- src/main/java/net/logstash/logback/marker/ObjectFieldsAppendingMarker.java
+++ src/main/java/net/logstash/logback/marker/ObjectFieldsAppendingMarker.java
@@ -14,25 +14,25 @@
 package net.logstash.logback.marker;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.lang.ObjectUtils;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+import com.fasterxml.jackson.databind.util.NameTransformer;
 
 /**
- * A marker that first converts an object to a {@link JsonNode}, and then
- * appends the fields of the {@link JsonNode} into the logstash event.
+ * A marker that "unwraps" the given object into the logstash event.
+ * i.e. The fields of the object are written inline into the logstash event
+ * similar to how the {@link com.fasterxml.jackson.annotation.JsonUnwrapped} annotation works.
  * <p>
- * Unless the object is already a {@link JsonNode), this may not be very efficient, so prefer using one of the other {@link LogstashMarker}s. It is included here for convenience where performance is
- * not a concern.
- * <p>
- * The object will be converted to a {@link JsonNode} via {@link ObjectMapper#convertValue(Object, Class)};
- * <p>
- * For example, if the converted JsonNode is
+ * For example, if the object is:
  * 
  * <pre>
  * {@code
@@ -63,6 +63,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  *     }
  * }
  * </pre>
+ * 
+ * Note that if the object cannot be unwrapped, then nothing will be written.
  */
 @SuppressWarnings("serial")
 public class ObjectFieldsAppendingMarker extends LogstashMarker {
@@ -70,6 +72,15 @@ public class ObjectFieldsAppendingMarker extends LogstashMarker {
     public static final String MARKER_NAME = LogstashMarker.MARKER_NAME_PREFIX + "OBJECT_FIELDS";
     
     private final Object object;
+    
+    /*
+     * Would really like to use Guava's Cache for these two, with expiring entries, soft reference, etc.
+     * But didn't want to introduce a dependency on Guava.
+     * 
+     * Since apps will typically serialize the same types of objects repeatedly, they shouldn't grow too much.
+     */
+    private static final ConcurrentHashMap<Class<?>, JsonSerializer<Object>> beanSerializers = new ConcurrentHashMap<Class<?>, JsonSerializer<Object>>();
+    private static final ConcurrentHashMap<ObjectMapper, SerializerProvider> serializerProviders = new ConcurrentHashMap<ObjectMapper, SerializerProvider>();
     
     public ObjectFieldsAppendingMarker(Object object) {
         super(MARKER_NAME);
@@ -79,15 +90,59 @@ public class ObjectFieldsAppendingMarker extends LogstashMarker {
     @Override
     public void writeTo(JsonGenerator generator, ObjectMapper mapper) throws IOException {
         if (object != null) {
-            JsonNode jsonNode = mapper.convertValue(object, JsonNode.class);
-            for (Iterator<Entry<String, JsonNode>> fields = jsonNode.fields(); fields.hasNext();) {
-                Entry<String, JsonNode> field = fields.next();
-                generator.writeFieldName(field.getKey());
-                generator.writeTree(field.getValue());
+            JsonSerializer<Object> serializer = getBeanSerializer(mapper);
+            if (serializer.isUnwrappingSerializer()) {
+                serializer.serialize(object, generator, getSerializerProvider(mapper));
             }
         }
     }
     
+    /**
+     * Gets a serializer that will write the {@link #object} unwrapped. 
+     */
+    private JsonSerializer<Object> getBeanSerializer(ObjectMapper mapper) throws JsonMappingException {
+        
+        JsonSerializer<Object> jsonSerializer = beanSerializers.get(object.getClass());
+        
+        if (jsonSerializer == null) {
+            JsonSerializer<Object> newSerializer = mapper.getSerializerFactory().createSerializer(
+                    getSerializerProvider(mapper),
+                    mapper.getSerializationConfig().constructType(object.getClass()))
+                .unwrappingSerializer(NameTransformer.NOP);
+            
+            JsonSerializer<Object> existingSerializer = beanSerializers.putIfAbsent(
+                    object.getClass(),
+                    newSerializer);
+            
+            jsonSerializer = (existingSerializer == null) ? newSerializer : existingSerializer;
+        }
+        return jsonSerializer;
+        
+    }
+
+    /**
+     * Gets a {@link SerializerProvider} configured with the {@link ObjectMapper}'s {@link SerializationConfig}
+     * ({@link ObjectMapper#getSerializationConfig()}) to be used for serialization.
+     * <p>
+     * Note that the {@link ObjectMapper}'s {@link SerializerProvider} ({@link ObjectMapper#getSerializerProvider()})
+     * cannot be used directly, because the {@link SerializerProvider}'s {@link SerializationConfig} ({@link SerializerProvider#getConfig()}) is null,
+     * which causes NullPointerExceptions when it is used.
+     */
+    private SerializerProvider getSerializerProvider(ObjectMapper mapper) {
+        
+        SerializerProvider provider = serializerProviders.get(mapper);
+        if (provider == null) {
+            
+            SerializerProvider newProvider = ((DefaultSerializerProvider) mapper.getSerializerProvider())
+                    .createInstance(mapper.getSerializationConfig(), mapper.getSerializerFactory());
+            
+            SerializerProvider existingProvider = serializerProviders.putIfAbsent(mapper, newProvider);
+            
+            provider = (existingProvider == null) ? newProvider : existingProvider;
+        }
+        return provider;
+    }
+
     @Override
     public boolean equals(Object obj) {
         if (!super.equals(obj)) {
