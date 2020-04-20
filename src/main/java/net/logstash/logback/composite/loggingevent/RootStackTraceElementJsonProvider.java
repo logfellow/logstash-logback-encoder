47d6982983920d734811a63eec64cc3646eb5aa2
Daniel Albuquerque <dmartinsalbuqu@expediagroup.com>
2020-04-20 14:00:00 +0100

    Add new json provider to log root stack trace elements (short names)

--- src/main/java/net/logstash/logback/composite/loggingevent/RootStackTraceElementJsonProvider.java
+++ src/main/java/net/logstash/logback/composite/loggingevent/RootStackTraceElementJsonProvider.java
@@ -2,9 +2,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +18,9 @@ import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.classic.spi.ThrowableProxy;
 import com.fasterxml.jackson.core.JsonGenerator;
 import net.logstash.logback.composite.AbstractFieldJsonProvider;
+import net.logstash.logback.composite.FieldNamesAware;
 import net.logstash.logback.composite.JsonWritingUtils;
+import net.logstash.logback.fieldnames.LogstashFieldNames;
 
 import java.io.IOException;
 
@@ -28,11 +30,13 @@ import java.io.IOException;
  *
  * @author Daniel Albuquerque
  */
-public class RootStackTraceElementJsonProvider extends AbstractFieldJsonProvider<ILoggingEvent> {
+public class RootStackTraceElementJsonProvider extends AbstractFieldJsonProvider<ILoggingEvent> implements FieldNamesAware<LogstashFieldNames> {
 
     public static final String FIELD_CLASS_NAME = "class_name";
     public static final String FIELD_METHOD_NAME = "method_name";
     public static final String FIELD_STACKTRACE_ELEMENT = "root_stack_trace_element";
+    private String fieldClassName = FIELD_CLASS_NAME;
+    private String fieldMethodName = FIELD_METHOD_NAME;
 
     public RootStackTraceElementJsonProvider() {
         setFieldName(FIELD_STACKTRACE_ELEMENT);
@@ -46,10 +50,24 @@ public class RootStackTraceElementJsonProvider extends AbstractFieldJsonProvider
                 StackTraceElement stackTraceElement = throwableProxy.getStackTraceElementProxyArray()[0].getStackTraceElement();
 
                 generator.writeObjectFieldStart(getFieldName());
-                JsonWritingUtils.writeStringField(generator, FIELD_CLASS_NAME, stackTraceElement.getClassName());
-                JsonWritingUtils.writeStringField(generator, FIELD_METHOD_NAME, stackTraceElement.getMethodName());
+                JsonWritingUtils.writeStringField(generator, fieldClassName, stackTraceElement.getClassName());
+                JsonWritingUtils.writeStringField(generator, fieldMethodName, stackTraceElement.getMethodName());
                 generator.writeEndObject();
             }
         }
+    }
+
+    @Override
+    public void setFieldNames(LogstashFieldNames fieldNames) {
+        setFieldClassName(fieldNames.getRootStackTraceElementClass());
+        setFieldMethodName(fieldNames.getRootStackTraceElementMethod());
+    }
+
+    private void setFieldClassName(String fieldClassName) {
+        this.fieldClassName = fieldClassName;
+    }
+
+    private void setFieldMethodName(String fieldMethodName) {
+        this.fieldMethodName = fieldMethodName;
     }
 }
--- src/test/java/net/logstash/logback/composite/loggingevent/RootStackTraceElementJsonProviderTest.java
+++ src/test/java/net/logstash/logback/composite/loggingevent/RootStackTraceElementJsonProviderTest.java
@@ -2,22 +2,22 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package net.logstash.logback.composite.loggingevent;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.classic.spi.StackTraceElementProxy;
 import ch.qos.logback.classic.spi.ThrowableProxy;
 import com.fasterxml.jackson.core.JsonGenerator;
+import net.logstash.logback.fieldnames.ShortenedFieldNames;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InOrder;
@@ -30,7 +30,7 @@ import java.io.IOException;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.when;
 
-public class ExceptionRootCauseMethodNameJsonProviderTest {
+public class RootStackTraceElementJsonProviderTest {
 
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
@@ -67,6 +67,25 @@ public class ExceptionRootCauseMethodNameJsonProviderTest {
         inOrder.verify(generator).writeObjectFieldStart(RootStackTraceElementJsonProvider.FIELD_STACKTRACE_ELEMENT);
         inOrder.verify(generator).writeStringField(RootStackTraceElementJsonProvider.FIELD_CLASS_NAME, "TestDeclaringClass");
         inOrder.verify(generator).writeStringField(RootStackTraceElementJsonProvider.FIELD_METHOD_NAME, "testMethodName");
+        inOrder.verify(generator).writeEndObject();
+    }
+
+    @Test
+    public void testOverrideFieldNameWithShortNames() throws IOException {
+        // GIVEN
+        when(event.getThrowableProxy()).thenReturn(throwableProxy);
+        StackTraceElementProxy[] steArray = new StackTraceElementProxy[]{steProxy};
+        when(throwableProxy.getStackTraceElementProxyArray()).thenReturn(steArray);
+        when(steProxy.getStackTraceElement()).thenReturn(ste);
+        provider.setFieldNames(new ShortenedFieldNames());
+        // WHEN
+        provider.writeTo(generator, event);
+        // THEN
+        InOrder inOrder = inOrder(generator);
+
+        inOrder.verify(generator).writeObjectFieldStart(RootStackTraceElementJsonProvider.FIELD_STACKTRACE_ELEMENT);
+        inOrder.verify(generator).writeStringField(ShortenedFieldNames.FIELD_CLASS, "TestDeclaringClass");
+        inOrder.verify(generator).writeStringField(ShortenedFieldNames.FIELD_METHOD, "testMethodName");
         inOrder.verify(generator).writeEndObject();
     }
 }
