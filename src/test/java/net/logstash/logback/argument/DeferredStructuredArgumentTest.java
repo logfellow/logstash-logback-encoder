e35a4dcd2d3220f9f78359fbe1be9eb2d4576de0
Phil Clay <philsttr@users.noreply.github.com>
2020-04-23 19:29:48 -0700

    Fixes #396, add StructuredArguments.defer, which is similar to Markers.defer

---
+++ src/test/java/net/logstash/logback/argument/DeferredStructuredArgumentTest.java
@@ -0,0 +1,87 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.logstash.logback.argument;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+
+public class DeferredStructuredArgumentTest {
+
+    private static final JsonFactory FACTORY = new MappingJsonFactory().enable(JsonGenerator.Feature.ESCAPE_NON_ASCII);
+
+    public static class MyClass {
+        private String myField;
+
+        public MyClass(String myField) {
+            this.myField = myField;
+        }
+
+        public String getMyField() {
+            return myField;
+        }
+
+        public void setMyField(String myField) {
+            this.myField = myField;
+        }
+    }
+
+    @Test
+    public void testWriteTo() throws IOException {
+
+        Supplier<MyClass> supplier = mock(Supplier.class);
+
+        when(supplier.get()).thenReturn(new MyClass("value"));
+
+        StringWriter writer = new StringWriter();
+        JsonGenerator generator = FACTORY.createGenerator(writer);
+
+        StructuredArgument structuredArgument = StructuredArguments.defer(() -> StructuredArguments.kv("myObject", supplier.get()));
+
+        verify(supplier, never()).get();
+
+        generator.writeStartObject();
+        structuredArgument.writeTo(generator);
+        generator.writeEndObject();
+        generator.flush();
+
+        verify(supplier).get();
+
+        assertThat(writer.toString()).isEqualTo("{\"myObject\":{\"myField\":\"value\"}}");
+
+        // execute again, to ensure that supplier is not invoked again
+        generator.writeStartObject();
+        structuredArgument.writeTo(generator);
+        generator.writeEndObject();
+        generator.flush();
+
+        verify(supplier).get();
+
+        assertThat(writer.toString()).isEqualTo("{\"myObject\":{\"myField\":\"value\"}} {\"myObject\":{\"myField\":\"value\"}}");
+
+    }
+
+}
