b1ec1db5d8ffaf8216fc78325d441150d9553a87
Bertrand Renuart <brenuart@users.noreply.github.com>
2021-07-07 18:40:16 +0200

    Update to license-maven-plugin:4.1 (#550)
    * Update to license-maven-plugin:4.1
    
    - Update POM to use the latest version of the Mycila license plugin (and correct GAV).
    - Move plugin configuration inside the <pluginManagement> section so the plugin can be manually invoked from the command line. Particularly handy to fix header issues automatically…
    - Change the build process to “check” the files only and fail the build if not valid.
    - Remove the custom lifecycle-mapping: latest versions of the plugin have support for M2E
    - Change comment style from JAVADOC to SLASHSTAR
    - Add copyright as first line
    - Update test case with a different expected hash caused by the additional lines in the license header
    
    Related issue: #549

--- src/test/java/net/logstash/logback/stacktrace/StackHasherTest.java
+++ src/test/java/net/logstash/logback/stacktrace/StackHasherTest.java
@@ -1,4 +1,6 @@
-/**
+/*
+ * Copyright 2013-2021 the original author or authors.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -151,7 +153,7 @@ public class StackHasherTest {
 
             // THEN
             Assertions.assertEquals(1, hashes.size());
-            Assertions.assertEquals("e30d4cae", hashes.getFirst());
+            Assertions.assertEquals("48abcbb0", hashes.getFirst());
         }
     }
 }
